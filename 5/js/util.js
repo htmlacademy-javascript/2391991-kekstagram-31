fe58ac1d2f726077b4862ccb2bdafec55b1686a2
Keks <keks@htmlacademy.ru>
2024-03-04 09:59:44 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/util.js
@@ -0,0 +1,27 @@
+// Создаем генератор случайных целых чисел
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+// Создает id
+const createId = (id) => function () {
+  return ++id;
+};
+
+// Генерируем id для переменных
+const generateId = createId (0);
+const generateUrl = createId(0);
+const generateMessage = createId(0);
+
+//  Создаем генератор случайных элементов
+const getRandomElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+export { getRandomInteger,
+  generateId,
+  generateUrl,
+  generateMessage,
+  getRandomElement,
+ };
