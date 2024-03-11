6d0afb2c3c66ff6c6de8f0a27c3c0205ad4ed3ea
Keks <keks@htmlacademy.ru>
2024-03-11 16:48:47 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/main.js
@@ -0,0 +1,8 @@
+import { similarUser } from './data.js';
+import'./util.js';
+import './photos/thumbnails.js';
+import './comments.js';
+import './photos/big-photo.js';
+
+// Данный вывод необходим для проверки
+console.log(similarUser);
---
+++ 7/js/util.js
@@ -0,0 +1,35 @@
+// Создаем генератор случайных целых чисел
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+// Создает id
+const createId = (id) => function () {
+  return ++id;
+};
+
+// Генерируем id для переменных
+const generateId = createId (0);
+const generateUrl = createId(0);
+const generateMessage = createId(0);
+
+//  Создаем генератор случайных элементов
+const getRandomElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+// Проверка нажатия клавиши Escape
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+// Проверка нажатия клавиши Enter
+const isEnterKey = (evt) => evt.key === 'Enter';
+
+export { getRandomInteger,
+  generateId,
+  generateUrl,
+  generateMessage,
+  getRandomElement,
+  isEscapeKey,
+  isEnterKey
+};
