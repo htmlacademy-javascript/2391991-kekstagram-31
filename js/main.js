8a8eb3a148c7be92134cecc734d2537445044ddc
Andrey Pyatkov <pyatkov10@mail.ru>
2024-04-08 18:41:22 +0500

    Вносит правки в код

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 import {showAlert} from './util.js';
 import { setUserFormSubmit } from './form-validation.js';
-import { clickHandlerPicture } from './photos/big-photo.js';
 import { configFilter } from './filter.js';
 import { renderPublicationPhotos } from './photos/thumbnails.js';
 import { getData } from './api.js';
@@ -8,12 +7,6 @@ import { getData } from './api.js';
 getData().then((photos) => {
   renderPublicationPhotos(photos);
   configFilter(photos);
-  const pictures = document.querySelectorAll('.picture');
-  pictures.forEach((picture, i) => {
-    picture.addEventListener('click', (evt) => {
-      clickHandlerPicture(evt, i, photos);
-    });
-  });
 })
   .catch((err)=>{
     showAlert(err.message);
